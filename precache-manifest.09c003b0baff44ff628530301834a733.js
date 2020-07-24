fbf0007f72c15167a68e563c92806b037165856f
keval8solanki <keval8solanki@gmail.com>
2020-07-24 22:10:17 +0530

    Updates

---
+++ precache-manifest.09c003b0baff44ff628530301834a733.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7b108807ebf9288f8420daf3212ba3",
+    "url": "/jobs-dashboard/index.html"
+  },
+  {
+    "revision": "939004aa9413ccb0f40e",
+    "url": "/jobs-dashboard/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "29bbc9eba84b082dfa87",
+    "url": "/jobs-dashboard/static/css/main.31e924e9.chunk.css"
+  },
+  {
+    "revision": "939004aa9413ccb0f40e",
+    "url": "/jobs-dashboard/static/js/2.74654003.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/jobs-dashboard/static/js/2.74654003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29bbc9eba84b082dfa87",
+    "url": "/jobs-dashboard/static/js/main.4fe5dd16.chunk.js"
+  },
+  {
+    "revision": "f4c7dc860407de061a9c",
+    "url": "/jobs-dashboard/static/js/runtime-main.7223f700.js"
+  },
+  {
+    "revision": "828fcf8b84fc9be9627d28ecc26b3ac1",
+    "url": "/jobs-dashboard/static/media/close.828fcf8b.svg"
+  },
+  {
+    "revision": "488045302a5d96e2af50bd0f0b127918",
+    "url": "/jobs-dashboard/static/media/exp.48804530.svg"
+  },
+  {
+    "revision": "23ca54fb5ad7b1101945bbe415edb8e7",
+    "url": "/jobs-dashboard/static/media/job.23ca54fb.svg"
+  },
+  {
+    "revision": "c1cca8542d0d75c733cec84425dd06e0",
+    "url": "/jobs-dashboard/static/media/loader.c1cca854.svg"
+  },
+  {
+    "revision": "9f46df7faeeaba3184744595564708a2",
+    "url": "/jobs-dashboard/static/media/location.9f46df7f.svg"
+  },
+  {
+    "revision": "3c4830662def54cdd8a97286ce649f8a",
+    "url": "/jobs-dashboard/static/media/people.3c483066.svg"
+  },
+  {
+    "revision": "cb6077d6926f12757db71b86c0eed9c3",
+    "url": "/jobs-dashboard/static/media/salary.cb6077d6.svg"
+  }
+]);
